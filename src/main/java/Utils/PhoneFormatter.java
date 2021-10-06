3132e1e7fa8ef9bb250fab0e6cfc21b69cf18cba
BOECHANGHORN <ahboe579@gmail.com>
2021-10-06 16:22:53 +0800

    all added

---
+++ src/main/java/Utils/PhoneFormatter.java
@@ -0,0 +1,26 @@
+package Utils;
+
+import javafx.scene.control.TextFormatter;
+
+import java.util.function.UnaryOperator;
+
+public class PhoneFormatter {
+    private final TextFormatter<String> textFormatter;
+
+    public PhoneFormatter() {
+        UnaryOperator<TextFormatter.Change> integerValidationFormatter = change -> {
+            if (change.getControlNewText().matches("\\+{0,1}\\d*")) {
+                return change;
+            } else {
+                return null;
+            }
+        };
+
+        this.textFormatter = new TextFormatter<>(integerValidationFormatter);
+    }
+
+    public TextFormatter<String> getInstance() {
+
+        return this.textFormatter;
+    }
+}
